08e569e331a6bf72e68e83390c3c2c286294b507
Abhi9125 <abhi.singh3132@gmail.com>
2025-05-14 03:15:24 +0530

    week_3.2_Hit DB by using the http server

--- week_3/index_3_2.js
+++ week_3/index_3_2.js
@@ -1,17 +1,40 @@
+const express = require("express");
 const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
 mongoose.connect(
   "mongodb+srv://abhisingh3132_cohort:Cohort_2@cluster0.qz24pt9.mongodb.net/username"
 );
 
 const User = mongoose.model("User", {
   name: String,
-  username: String,
+  email: String,
   password: String,
 });
 
-const kitty = new User({
-  name: "abhi",
-  username: "abhi3132",
-  password: "pass",
+app.post("/signup", async (req, res) => {
+  const { name, username, password } = req.body;
+
+  // Read the user data
+  const userExist = await User.findOne({ email: username });
+
+  if (userExist) {
+    res.send("User already exist");
+  }
+
+  //  Write the user data
+  const user = new User({
+    name: name,
+    email: username,
+    password: password,
+  });
+
+  user.save();
+  res.json({
+    msg: "data save",
+  });
+  console.log("done");
 });
-kitty.save().then(() => console.log("Data Save"));
+
+app.listen(3000);
