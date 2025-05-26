614c29e4c53f1983a5a8f968aa0f3c2345113296
Abhi9125 <abhi.singh3132@gmail.com>
2025-05-26 16:46:07 +0530

    Week_4.1 learn debounce and write code

--- week_4/index.js
+++ week_4/index.js
@@ -1,6 +1,14 @@
-function calculateSum() {
-  const num1 = document.getElementById("input1").value;
-  const num2 = document.getElementById("input2").value;
-  const sum = document.getElementById("finalSum");
-  sum.innerHTML = Number(num1) + Number(num2);
+// function calculateSum() {
+//   const num1 = document.getElementById("input1").value;
+//   const num2 = document.getElementById("input2").value;
+//   const sum = document.getElementById("finalSum");
+//   sum.innerHTML = Number(num1) + Number(num2);
+// }
+
+let timeout;
+function debouncetest() {
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+    console.log("a");
+  }, 1000);
 }
