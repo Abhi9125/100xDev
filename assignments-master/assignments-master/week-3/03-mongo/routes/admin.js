2dec64e76ea2382bb42ef9a80c53f9516b3745f1
Abhi9125 <abhi.singh3132@gmail.com>
2025-06-18 11:51:37 +0530

    Week_3 assignment_03_mongo_complete

--- assignments-master/assignments-master/week-3/03-mongo/routes/admin.js
+++ assignments-master/assignments-master/week-3/03-mongo/routes/admin.js
@@ -1,18 +1,57 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post("/signup", async (req, res) => {
+  // Implement admin signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+
+  await Admin.create({
+    username,
+    password,
+  });
+
+  res.json({
+    message: "Admin created successfully",
+  });
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post("/courses", adminMiddleware, async (req, res) => {
+  // Implement course creation logic
+
+  const { title, description, price, imageLink } = req.body;
+
+  const courseCreationRes = await Course.create({
+    title,
+    description,
+    price,
+    imageLink,
+  });
+
+  console.log(courseCreationRes);
+
+  res.json({
+    message: "Course created successfully",
+    courseId: courseCreationRes._id,
+  });
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get("/courses", adminMiddleware, (req, res) => {
+  // Implement fetching all courses logic
+  Course.find().then(function (response) {
+    if (response) {
+      res.json({
+        AllCourses: response,
+      });
+    } else {
+      res.json({
+        msg: "No course is available",
+      });
+    }
+  });
 });
 
 module.exports = router;
--- assignments-master/assignments-master/week-3/03-mongo/routes/user.js
+++ assignments-master/assignments-master/week-3/03-mongo/routes/user.js
@@ -1,22 +1,83 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
 
 // User Routes
-router.post('/signup', (req, res) => {
-    // Implement user signup logic
+router.post("/signup", async (req, res) => {
+  // Implement user signup logic
+  const { username, password } = req.body;
+
+  await User.create({
+    username,
+    password,
+  });
+
+  res.json({
+    message: "User created Successfully",
+  });
 });
 
-router.get('/courses', (req, res) => {
-    // Implement listing all courses logic
+router.get("/courses", async (req, res) => {
+  // Implement listing all courses logic
+  try {
+    const response = await Course.find();
+
+    res.json({
+      courses: response,
+    });
+  } catch (err) {
+    res.json({
+      msg: "Courses are not available",
+      err,
+    });
+  }
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
-    // Implement course purchase logic
+router.post("/courses/:courseId", userMiddleware, async (req, res) => {
+  // Implement course purchase logic
+
+  const courseId = req.params.courseId;
+  console.log("courseId from params:", courseId);
+
+  const username = req.headers.username;
+  try {
+    await User.updateOne(
+      {
+        username,
+      },
+      {
+        $push: {
+          purchaseCourses: courseId,
+        },
+      }
+    );
+
+    res.json({
+      message: "Course purchased successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
+router.get("/purchasedCourses", userMiddleware, async (req, res) => {
+  // Implement fetching purchased courses logic
+  const userprofile = await User.findOne({
+    username: req.headers.username,
+  });
+  console.log(userprofile);
+
+  const allPurchaseCourseList = await Course.find({
+    _id: {
+      $in: userprofile.purchaseCourses,
+    },
+  });
+
+  res.json({
+    coursePurchase: allPurchaseCourseList,
+  });
 });
 
-module.exports = router
+module.exports = router;
