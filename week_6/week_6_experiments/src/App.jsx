1dee4c950ea2577be7dc6efe58cd15604a225ac4
Abhi9125 <abhi.singh3132@gmail.com>
2025-06-21 13:39:01 +0530

    Week_6.1_Lecture _first experiment done

---
+++ week_6/week_6_experiments/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Header from "./component/Header";
+
+function App() {
+  return (
+    <div>
+      <Header title={`Header Compoent1`} />
+      <Header title={`Header Compoent2`} />
+    </div>
+  );
+}
+
+export default App;
