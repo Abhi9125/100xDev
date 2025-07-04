6229c6a6ba8694e337587cc13e045c8647105c2c
Abhi9125 <abhi.singh3132@gmail.com>
2025-07-05 00:18:50 +0530

    Assignment_week_6 Done

--- assignments-master/assignments-master/week-6/2-use-callback/src/components/Assignment1.jsx
+++ assignments-master/assignments-master/week-6/2-use-callback/src/components/Assignment1.jsx
@@ -1,31 +1,42 @@
+import { useCallback } from "react";
+import { memo } from "react";
 import { useState } from "react";
 
 // Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
 
 export function Assignment1() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    // Your code starts here
-    function handleIncrement() {
+  // Your code starts here
+  const handleIncrement = useCallback(() => {
+    console.log("increase");
 
-    }
+    setCount((count) => count + 1);
+  }, []);
 
-    function handleDecrement() {
-        
-    }
-    // Your code ends here
+  const handleDecrement = useCallback(() => {
+    console.log("Decrease");
+    setCount((count) => count - 1);
+  }, []);
+  // Your code ends here
 
-    return (
-        <div>
-            <p>Count: {count}</p>
-            <CounterButtons onIncrement={handleIncrement} onDecrement={handleDecrement} />
-        </div>
-    );
-};
-
-const CounterButtons = ({ onIncrement, onDecrement }) => (
+  return (
     <div>
-        <button onClick={onIncrement}>Increment</button>
-        <button onClick={onDecrement}>Decrement</button>
+      <p>Count: {count}</p>
+      <CounterButtons
+        onIncrement={handleIncrement}
+        onDecrement={handleDecrement}
+      />
     </div>
-);
+  );
+}
+
+const CounterButtons = memo(({ onIncrement, onDecrement }) => (
+  <div>
+    <button onClick={onIncrement}>Increment</button>
+
+    <br />
+    <br />
+    <button onClick={onDecrement}>Decrement</button>
+  </div>
+));
--- assignments-master/assignments-master/week-6/3-use-ref/src/components/Assignment2.jsx
+++ assignments-master/assignments-master/week-6/3-use-ref/src/components/Assignment2.jsx
@@ -1,19 +1,45 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback } from "react";
+import { useRef } from "react";
 
 // Create a component that tracks and displays the number of times it has been rendered. Use useRef to create a variable that persists across renders without causing additional renders when it changes.
 
+/* This Q done by mutiple ways
+1. By creating a state varibale - But the problem in this approach is if we put mutiple setState is show only one count but in actual happen 2 count. 
 export function Assignment2() {
-    const [, forceRender] = useState(0);
+  const [, forceRender] = useState(0);
+  const [count, setCount] = useState(0);
+  const handleReRender = () => {
+    // Update state to force re-render
+    forceRender(Math.random());
+    setCount(count + 1);
+  };
 
-    const handleReRender = () => {
-        // Update state to force re-render
-        forceRender(Math.random());
-    };
+  return (
+    <div>
+      <p>This component has rendered {count} times.</p>
+      <button onClick={handleReRender}>Force Re-render</button>
+    </div>
+  );
 
-    return (
-        <div>
-            <p>This component has rendered {0} times.</p>
-            <button onClick={handleReRender}>Force Re-render</button>
-        </div>
-    );
-};
+2. By creating the GLobal Variable. THis is also not a good idea.
+3. By using the useRef - useRef not only use for the referencing the elemnt but also use 
+for holding value in re rendering. 
+}
+
+*/
+export function Assignment2() {
+  const [, forceRender] = useState(0);
+  const RenderCount = useRef(1);
+  const handleReRender = () => {
+    // Update state to force re-render
+    RenderCount.current = RenderCount.current + 1;
+    forceRender(Math.random());
+  };
+
+  return (
+    <div>
+      <p>This component has rendered {RenderCount.current} times.</p>
+      <button onClick={handleReRender}>Force Re-render</button>
+    </div>
+  );
+}
