772e19fa970bff4529281e310e07aced4e0f8795
Abhi9125 <abhi.singh3132@gmail.com>
2025-07-22 03:08:10 +0530

    Paytm_Frontend_user page

--- paytm-main/frontend/src/component/Users.jsx
+++ paytm-main/frontend/src/component/Users.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Button from "./Button";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Users = () => {
   // Replace with backend call
@@ -39,6 +40,7 @@ const Users = () => {
 export default Users;
 
 function User({ user }) {
+  const navigate = useNavigate();
   return (
     <div className="flex justify-between">
       <div className="flex">
@@ -54,7 +56,12 @@ function User({ user }) {
         </div>
       </div>
 
-      <div className="flex flex-col justify-center h-ful">
+      <div
+        className="flex flex-col justify-center h-ful"
+        onClick={() => {
+          navigate("/send?id=" + user._id + "&name=" + user.firstName);
+        }}
+      >
         <Button label={"Send money"} />
       </div>
     </div>
