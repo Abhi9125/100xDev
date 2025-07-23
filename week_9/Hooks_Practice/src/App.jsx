d2579f97d1db7ad4572ceee7afcefd9849f4959d
Abhi9125 <abhi.singh3132@gmail.com>
2025-07-24 00:26:25 +0530

    lecture_9.1_useEffect life cycke and how to handle infinity call

--- week_9/Hooks_Practice/src/App.jsx
+++ week_9/Hooks_Practice/src/App.jsx
@@ -1,51 +1,67 @@
 import { useEffect, useState } from "react";
-import React from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import axios from "axios";
 
-function App() {
-  const [render, setRender] = useState(true);
+function useTodos(n) {
+  const [todos, setTodos] = useState([]);
+  const [simmer, setSimmer] = useState(true);
+
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     axios.get("http://localhost:8080/todos").then((res) => {
+  //       setTodos(res.data.todos);
+  //       setSimmer(false);
+  //     });
+  //   }, n * 1000);
+
+  //   axios.get("http://localhost:8080/todos").then((res) => {
+  //     setTodos(res.data.todos);
+  //     setSimmer(false);
+  //   });
+  // }, [n]);
+
+  // ----------------------------------
+  /* but one problem in the above code what if 'N' is dynamic. example inisily 'N' = 5, so 
+  'N' is 5 setInterval timer attach with 5sec and then after sometime 'N' = 8 but the time with 
+  setInterval is 5 so to fix this consition we clear previous setInterval. 
+  */
 
   useEffect(() => {
-    setTimeout(() => {
-      setRender(false);
-    }, 10000);
-  }, []);
-  return <>{render ? <MyComponent /> : <div></div>}</>;
+    const timer = setInterval(() => {
+      axios.get("http://localhost:8080/todos").then((res) => {
+        setTodos(res.data.todos);
+        setSimmer(false);
+      });
+    }, n * 1000);
+
+    axios.get("http://localhost:8080/todos").then((res) => {
+      setTodos(res.data.todos);
+      setSimmer(false);
+    });
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [n]);
+
+  return [todos, simmer];
+}
+
+function App() {
+  const [todos, simmer] = useTodos(5);
+
+  return (
+    <>{simmer ? "Loading..." : todos.map((todo) => <Track todo={todo} />)}</>
+  );
+}
+
+function Track({ todo }) {
+  return (
+    <div>
+      {todo.title}
+      <br />
+      {todo.description}
+    </div>
+  );
 }
 
 export default App;
-
-// function MyComponent() {
-//   useEffect(() => {
-//     // Perform setup or data fetching here
-//     console.log("component Mounting");
-
-//     return () => {
-//       // Cleanup code (similar to componentWillUnmount)
-//       console.log("unMounting Component");
-//     };
-//   }, []);
-
-//   // Render UI
-//   return <div>hello</div>;
-// }
-
-// Class base component life cycle
-
-class MyComponent extends React.Component {
-  componentDidMount() {
-    console.log("Mounting");
-  }
-
-  componentWillUnmount() {
-    // Clean up (e.g., remove event listeners or cancel subscriptions)
-    console.log("unMouting");
-  }
-
-  render() {
-    // Render UI
-    return <div>Hello </div>;
-  }
-}
