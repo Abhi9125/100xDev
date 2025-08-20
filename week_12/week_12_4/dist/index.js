aa6a7a32d8a04d0abf8256ca4d3184c63567073d
Abhi9125 <abhi.singh3132@gmail.com>
2025-08-20 16:51:33 +0530

    Lecture_12_4_practice_realtionship and join in postgres

--- week_12/week_12_4/dist/index.js
+++ week_12/week_12_4/dist/index.js
@@ -14,25 +14,89 @@ const pg_1 = require("pg");
 const client = new pg_1.Client({
     connectionString: "postgresql://postgres:mysecretpassword@localhost/postgres",
 });
-// Insert data unsecure way in this way intruder insert the sql commant and distroy the database it called sql injection to resolve this problem
-// we use the 2nd way to insert the data
-// 1 way
-/* async function InserData(username: string, email: string, password: string) {
+/* async function createAndInsertTable() {
   await client.connect();
-  const result = await client.query(`
-        INSERT INTO users (username, email, password)
-        VALUES ('${username}', '${email}', '${password}')
+  await client.query(`
+    DROP TABLE IF EXISTS addresses2;
     `);
-  console.log(result);
-} */
-// 2 way
-function InserData(username, email, password) {
+
+  console.log("Tabe drop");
+  await client.query(`
+    CREATE TABLE addresses(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    country VARCHAR(100) NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    pincode VARCHAR(20),
+    create_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+   )
+  `);
+
+  console.log("Addresses2 created");
+
+  await client.query(`
+    INSERT INTO addresses(user_id , city , country, street, pincode)
+    VALUES (3, 'Robertsganj', 'India', 'CivilLine', '231216')
+  `);
+
+  console.log("Table Inserted");
+}
+
+createAndInsertTable(); */
+// ---- Get address details using user id
+/* async function getAddressesByUserId(userId: number) {
+  await client.connect();
+
+  const result = await client.query(
+    `SELECT * FROM addresses WHERE user_id = $1`,
+    [userId]
+  );
+
+  console.log("get addresses detail");
+
+  console.log(result.rows);
+}
+
+getAddressesByUserId(3);
+ */
+/* async function InsertValue(userId: number) {
+  await client.connect();
+
+  await client.query(`
+    INSERT INTO addresses(user_id , city , country, street, pincode)
+    VALUES (3, 'Robertsganj11', 'India11', 'CivilLine11', '231216')
+  `);
+
+  console.log("Table Inserted");
+}
+
+InsertValue(3); */
+// Joins
+function GettingAddressByJoin(userId) {
     return __awaiter(this, void 0, void 0, function* () {
-        yield client.connect();
-        const result = yield client.query(`
-        INSERT INTO users (username, email, password)
-        VALUES ($1, $2, $3)`, [username, email, password]);
-        console.log(result);
+        try {
+            yield client.connect();
+            const query = `
+    SELECT u.id, u.username,  u.email, a.city, a.country, a.street, a.pincode
+    FROM users u
+    FULL JOIN addresses a on u.id = a.user_id
+    WHERE u.id= $1
+    `;
+            const result = yield client.query(query, [userId]);
+            if (result.rows.length > 0) {
+                console.log("User address found : ", result.rows);
+                return result.rows;
+            }
+            else {
+                console.log("No user or address found with the given ID.");
+                return null;
+            }
+        }
+        catch (err) {
+            console.error("Error during fetching", err);
+        }
     });
 }
-InserData("abhi12345", "abhi112345.singh@gmail.com", "dlkfjsd12345");
+GettingAddressByJoin(1);
--- week_12/week_12_4/src/index.ts
+++ week_12/week_12_4/src/index.ts
@@ -5,29 +5,92 @@ const client = new Client({
   connectionString: "postgresql://postgres:mysecretpassword@localhost/postgres",
 });
 
-// Insert data unsecure way in this way intruder insert the sql commant and distroy the database it called sql injection to resolve this problem
-// we use the 2nd way to insert the data
-
-// 1 way
-/* async function InserData(username: string, email: string, password: string) {
+/* async function createAndInsertTable() {
   await client.connect();
-  const result = await client.query(`
-        INSERT INTO users (username, email, password)
-        VALUES ('${username}', '${email}', '${password}')
+  await client.query(`
+    DROP TABLE IF EXISTS addresses2;
     `);
-  console.log(result);
-} */
 
-// 2 way
-async function InserData(username: string, email: string, password: string) {
-  await client.connect();
-  const result = await client.query(
-    `
-        INSERT INTO users (username, email, password)
-        VALUES ($1, $2, $3)`,
-    [username, email, password]
-  );
-  console.log(result);
+  console.log("Tabe drop");
+  await client.query(`
+    CREATE TABLE addresses(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    country VARCHAR(100) NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    pincode VARCHAR(20),
+    create_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+   )
+  `);
+
+  console.log("Addresses2 created");
+
+  await client.query(`
+    INSERT INTO addresses(user_id , city , country, street, pincode)
+    VALUES (3, 'Robertsganj', 'India', 'CivilLine', '231216')
+  `);
+
+  console.log("Table Inserted");
 }
 
-InserData("abhi12345", "abhi112345.singh@gmail.com", "dlkfjsd12345");
+createAndInsertTable(); */
+
+// ---- Get address details using user id
+
+/* async function getAddressesByUserId(userId: number) {
+  await client.connect();
+
+  const result = await client.query(
+    `SELECT * FROM addresses WHERE user_id = $1`,
+    [userId]
+  );
+
+  console.log("get addresses detail");
+
+  console.log(result.rows);
+}
+
+getAddressesByUserId(3);
+ */
+
+/* async function InsertValue(userId: number) {
+  await client.connect();
+
+  await client.query(`
+    INSERT INTO addresses(user_id , city , country, street, pincode)
+    VALUES (3, 'Robertsganj11', 'India11', 'CivilLine11', '231216')
+  `);
+
+  console.log("Table Inserted");
+}
+
+InsertValue(3); */
+
+// Joins
+
+async function GettingAddressByJoin(userId: number) {
+  try {
+    await client.connect();
+    const query = `
+    SELECT u.id, u.username,  u.email, a.city, a.country, a.street, a.pincode
+    FROM users u
+  JOIN addresses a on u.id = a.user_id
+    WHERE u.id= $1
+    `;
+
+    const result = await client.query(query, [userId]);
+
+    if (result.rows.length > 0) {
+      console.log("User address found : ", result.rows);
+      return result.rows;
+    } else {
+      console.log("No user or address found with the given ID.");
+      return null;
+    }
+  } catch (err) {
+    console.error("Error during fetching", err);
+  }
+}
+GettingAddressByJoin(1);
