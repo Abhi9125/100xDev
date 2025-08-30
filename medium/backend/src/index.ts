ebd07ca622358051a867a9ece9602107366e113a
Abhi9125 <abhi.singh3132@gmail.com>
2025-08-31 02:41:42 +0530

    Project_add_cors

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,12 +1,14 @@
 import { Hono } from "hono";
 import { user } from "./routes/user";
 import { BlogPostRoute } from "./routes/blog";
+import { cors } from "hono/cors";
+
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
   };
 }>();
-
+app.use("/*", cors());
 app.route("/api/v1/user", user);
 app.route("api/v1/blog", BlogPostRoute);
 export default app;
