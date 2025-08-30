77a4a485c3b4371ddac9f3bbf5f97c1f02dcd7a6
Abhi9125 <abhi.singh3132@gmail.com>
2025-08-30 17:45:29 +0530

    Project_Frontend_init

---
+++ medium/frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { Blog } from "./pages/Blog";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Signin />} />
+          <Route path="/singup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
