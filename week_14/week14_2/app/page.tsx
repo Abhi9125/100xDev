386fb00960a7b4388852ee4abc805dc2a1c448f4
Abhi9125 <abhi.singh3132@gmail.com>
2025-09-07 02:38:47 +0530

    Lecture_14_2_signup_route_done

--- week_14/week14_2/app/page.tsx
+++ week_14/week14_2/app/page.tsx
@@ -1,21 +1,6 @@
 import Image from "next/image";
 import axios from "axios";
 
-async function getData() {
-  const res = await axios.get(
-    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
-  );
-
-  return res.data;
-}
-
 export default async function Home() {
-  const userData = await getData();
-  console.log(userData);
-  return (
-    <div>
-      Name : {userData.name}
-      <p>{userData.email}</p>
-    </div>
-  );
+  return <div>hii there!!</div>;
 }
