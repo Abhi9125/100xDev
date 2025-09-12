a5d4537af6515fc9db0babb89487586bf2f59ba8
Abhi9125 <abhi.singh3132@gmail.com>
2025-09-12 17:20:27 +0530

    Project_update the Quote

--- medium/frontend/src/components/Quotes.tsx
+++ medium/frontend/src/components/Quotes.tsx
@@ -2,11 +2,11 @@ export const Quotes = () => {
   return (
     <div className="max-w-lg text-center p-6">
       <div className="text-xl italic text-gray-700">
-        "The customer service i receive was exceptional.The support team went
-        above and beyond to address my concern."
+        "Great blogs don’t just tell stories, they plant seeds of curiosity in
+        the reader's mind."
       </div>
-      <div className="mt-4 text-gray-500 font-semibold">Jules Windfiels</div>
-      <div className="text-gray-500">CEO, Acmo Inc.</div>
+      <div className="mt-4 text-gray-500 font-semibold">Abhishek Singh</div>
+      <div className="text-gray-500">Full Stack Developer</div>
     </div>
   );
 };
