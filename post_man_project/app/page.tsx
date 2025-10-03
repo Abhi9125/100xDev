a71a905a9377dcc955a5d376033674a3d34104f5
Abhi9125 <abhi.singh3132@gmail.com>
2025-10-04 00:44:17 +0530

    PostaMan_project_init_install_Nextjs and shadcn

---
+++ post_man_project/app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <Button className="flex flex-col justify-center content-center">Add</Button>
+  );
+}
