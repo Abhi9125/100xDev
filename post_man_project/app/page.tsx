01127c532100213e2ddb7253c67b5203155773a6
Abhi9125 <abhi.singh3132@gmail.com>
2025-11-02 13:24:05 +0530

    delete_files

--- post_man_project/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <Button className="flex flex-col justify-center content-center">Add</Button>
-  );
-}
