3c9e05ca49de0054ca3611fee5ada2cb2cdbcd94
Abhi9125 <abhi.singh3132@gmail.com>
2025-11-06 17:52:48 +0530

    Lecture_19_2_Know_how_redisQueue_comminicate_with worker

---
+++ week_19/week_19_2/express_backend/dist/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { createClient } from "redis";
+const app = express();
+app.use(express.json());
+const client = createClient();
+client.on("error", (err) => console.log("Redis Client Err", err));
+app.post("/submit", async (req, res) => {
+    const { problemId, code, language } = req.body;
+    console.log(JSON.stringify({ problemId, code, language }));
+    try {
+        await client.lPush("problems", JSON.stringify({ problemId, code, language }));
+        res.status(200).send("Submission received and stored");
+    }
+    catch (err) {
+        console.error("Redis Error: ", err);
+        res.status(500).send("Failed to store submission");
+    }
+});
+async function startServer() {
+    try {
+        await client.connect();
+        console.log("connected to redis");
+        app.listen(3000, () => {
+            console.log("server is running");
+        });
+    }
+    catch (err) {
+        console.error("Failed to connect to redis", err);
+    }
+}
+startServer();
+//# sourceMappingURL=index.js.map
---
+++ week_19/week_19_2/workers/dist/index.js
@@ -0,0 +1,34 @@
+import { createClient } from "redis";
+const client = createClient();
+async function processSubmission(submission) {
+    const { problemId, code, language } = JSON.parse(submission);
+    console.log("problemId", problemId);
+    console.log("code", code);
+    console.log("language", language);
+    await new Promise((res, rej) => {
+        setTimeout(res, 1000);
+    });
+    console.log(`Finished processing submission for problemId ${problemId}.`);
+}
+async function startWorker() {
+    try {
+        await client.connect();
+        console.log("Worker connected to redis");
+        //Mian loop
+        while (true) {
+            try {
+                const submission = await client.brPop("problems", 0);
+                const res = await processSubmission(submission.element);
+                console.log(res);
+            }
+            catch (err) {
+                console.log("Error PRocessing the submmision");
+            }
+        }
+    }
+    catch (err) {
+        console.log("error conneting the redis");
+    }
+}
+startWorker();
+//# sourceMappingURL=index.js.map
